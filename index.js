697efdf764bc366f25b14661a99614cbcd7f53a5
afnanalansari <afnan.alansari111@gmail.com>
2023-09-11 18:59:36 +0300

    task solved

--- index.js
+++ index.js
@@ -11,7 +11,11 @@
 
 // Part 1 answer 👇🏻 ...
 
-
+let fullName = "afnan alansari";
+let yearOfBirth = 1999;
+let hobby = "baseball";
+let funFact = "I am a scorpio!";
+const image = 'https://i.pinimg.com/originals/7c/0c/9a/7c0c9a680a334565ceaaaaf39cabf23a.jpg';
 
 
 
@@ -29,9 +33,9 @@
  * You can uncomment a line by removing the // at the start
  */
 
-// const fullNameString = `My name is ${}`;
-// const yearOfBirthString =
-// const hobbyString =
+ const fullNameString = `My name is ${fullName}`;
+ const yearOfBirthString = `I am ${2023-yearOfBirth} years old`;
+ const hobbyString = `My hobby is ${hobby}`;
 
 
 
@@ -45,14 +49,18 @@ let stars = 0;
 
 function incrementBy1() {
   // Increment stars by 1 👇🏻
+  stars++;
 }
 function decrementBy1() {
   // decrement stars by 1 👇🏻
+  stars--;
 }
 
 function incrementBy2() {
   // Increment stars by 2 👇🏻
+  stars+=2;
 }
 function decrementBy2() {
   // decrement stars by 2 👇🏻
+  stars-=2;
 }
